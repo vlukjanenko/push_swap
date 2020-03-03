546d0fd706f61de2a3393523d9174cb58df1d661
Magan Josue <majosue@at-k1.21-school.ru>
2020-03-03 11:11:53 +0300

    another

--- ft_1st_pass.c
+++ ft_1st_pass.c
@@ -6,7 +6,7 @@
 /*   By: majosue <majosue@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/26 11:47:47 by majosue           #+#    #+#             */
-/*   Updated: 2020/02/29 13:12:33 by majosue          ###   ########.fr       */
+/*   Updated: 2020/02/29 16:47:57 by majosue          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -95,7 +95,7 @@ static void ft_rotate(t_list **a, t_list **b, int el, int steps)
 	min = ft_find_min(*b, el);
 	pos = ft_get_pos(*b, min);
 	q = ft_get_number_of_elements(*b);
-	if (pos < q / 2 && steps >= pos && *b && min != ((t_elm*)(*b)->content)->index)	
+	if (pos < q / 2 && steps <= pos && *b && min != ((t_elm*)(*b)->content)->index)	
 			ft_rr(a, b, 1);
 		else 
 			ft_ra(a, b, 1);
@@ -110,7 +110,7 @@ static void ft_rrotate(t_list **a, t_list **b, int el, int steps)
 	min = ft_find_min(*b, el);
 	pos = ft_get_pos(*b, min);
 	q = ft_get_number_of_elements(*b);
-	if (pos >= q / 2 && steps >= q - pos && *b && min != ((t_elm*)(*b)->content)->index)
+	if (pos >= q / 2 && steps <= q - pos && *b && min != ((t_elm*)(*b)->content)->index)
 		{	
 				ft_rrr(a, b, 1);
 		}
@@ -128,14 +128,43 @@ int  ft_get_el(t_list *a, int n)
 return (((t_elm*)a->content)->index);
 }
 
+
+
+static void ft_correctb(t_list **a, t_list **b, int el)
+{
+	int q;
+	int pos;
+	int min = 0;
+
+	min = ft_find_min(*b, el);
+	pos = ft_get_pos(*b, min);
+	q = ft_get_number_of_elements(*b);
+		if (pos < q / 2)
+		{
+			pos++;
+			while (--pos > 0)
+				ft_rb(a, b, 1);
+		}
+		else
+		{
+			pos--;
+			while (++pos < q)
+				{
+						ft_rrb(a, b, 1);
+				}
+		}
+}
+
 void	ft_push_chunk_to_b(t_list **a, t_list **b, t_sort *n)
 {
+	int el;
+	
 	n->n = ft_get_number_of_elements(*a);
 	if (n->meet_f <= n->n - n->meet_l)
 	{
 		while (n->meet_f > 0)
 		{
-			ft_rotate(a, b, ft_get_el(*a, n->meet_f), n->meet_f);
+			ft_rotate(a, b, (el = ft_get_el(*a, n->meet_f)), n->meet_f);
 			n->meet_f--;
 		}	
 	}
@@ -143,10 +172,11 @@ void	ft_push_chunk_to_b(t_list **a, t_list **b, t_sort *n)
 	{
 		while (n->meet_l < n->n)
 		{
-			ft_rrotate(a, b, ft_get_el(*a, n->meet_f), n->meet_l);
+			ft_rrotate(a, b, (el = ft_get_el(*a, n->meet_f)), n->meet_l);
 			n->meet_l++;
 		}
 	}
+	ft_correctb(a, b, el);
 	ft_pb(a, b, 1);
 }
 
